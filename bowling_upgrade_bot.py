752e322f15b04662320b440cc08451cdb3a10026
nexoraiztop <nexoraiztop@gmail.com>
2026-09-02 15:56:07 +0600

    Update rewards list with new NFT link
    Removed old NFT reward links and added a new one.

--- bowling_upgrade_bot.py
+++ bowling_upgrade_bot.py
@@ -46,8 +46,7 @@ INFINITE_BALANCE_USERNAMES = ["Nexoraizfuck"]
 
 # Ссылки на NFT-подарки (финальный уровень лестницы призов)
 rewards_list = [
-    "http://t.me/nft/ViceCream-157848",
-    "http://t.me/nft/ViceCream-112371",
+    "https://t.me/nft/ChillFlame-64612"
 ]
 
 # ID премиум-эмодзи, по местам использования
